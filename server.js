3353d360295b417e953095d704821d5a769dc239
PremKumar R <145741603+premkumar118@users.noreply.github.com>
2023-10-15 17:43:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,208 @@
+let content=[
+    {
+        "image" : "https://i.ytimg.com/vi/TmPqLJw5XSo/hq720_live.jpg?sqp=CPyPvKgG-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDguKOZeONZdTEhxIcD9GRM5rxxcw",
+        "title" : "Hey MADAN bro diiiiiiii | #bgmi #bgmilive #madanop #madan #bts",
+        "name" : "M A D A N",
+        "views" : "1.9K watching &bull; LIVE",
+        "link" : "https://www.youtube.com/watch?v=LH7lnRON6kk"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/hgWKn-QpVis/hq720_live.jpg?sqp=CMjpuqgG-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARLRqIdI1YXDTYOTQ_jCHfpyq7aA",
+        "title" : "BGMI GAMEPLAY LIVE IN TAMIL WITH JOD PLAYER |#bgmi #Tamil #Vaadhi #ravenSharp #pubg",
+        "name" : "RAVEN SHARP YOUTUBE",
+        "views" : "100k views &bull; 1month",
+        "link" : "https://www.youtube.com/watch?v=jtF2JvRvMRU"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/qGq9-TYyzYo/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB-7ppX7emR93agUwnnL3__M3FOKQ",
+        "title" : "DOG Paavangal | Parithabangal",
+        "name" : "PARITHABANGAL",
+        "views" : "246k views &bull; 1 hour ago",
+        "link" : "https://www.youtube.com/watch?v=qGq9-TYyzYo"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/4ykAepVkG5Y/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDnDyytlY1H9v7pj54iUHNaoJaiog",
+        "title" : "Create A Website Like YouTube Using HTML CSS JS | YouTube Clone Website Design Step By Step",
+        "name" : "GreatStack",
+        "views" : "473k views &bull; 2 years ago",
+        "link" : "https://www.youtube.com/watch?v=4ykAepVkG5Y"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/iWWox240TpI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAUIteWCuK4I9mT8HMTikdEDMuyXw",
+        "title" : "Call of Duty : Warzone Caldera | RTX 4090 24GB ( 4K Maximum Settings RTX ON / DLSS OFF )",
+        "name" : "GTX 1050 Ti",
+        "views" : "1.3M views &bull; 4 months ago",
+        "link" : "https://www.youtube.com/watch?v=iWWox240TpI&t=13s"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/aqLAfX0cuvk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDIFP28byoSek90vfkadqLMIzg_qw",
+        "title" : "LEVINHO BACK ON MOBILE 🔥 PUBG MOBILE",
+        "name" : "Levinho",
+        "views" : "109K views &bull; 1 day ago",
+        "link" : "https://www.youtube.com/watch?v=aqLAfX0cuvk"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/44R9ono-jgE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCZlDrAM4R2ZX13vzOr0oxrRijjRg",
+        "title" : "A2D Channel Hacked? 😭 - Plz Help Me! | Hacker Vs A2D - PC Doc",
+        "name" : "A2D Channel",
+        "views" : "32K views &bull; 46 minutes ago",
+        "link" : "https://www.youtube.com/watch?v=44R9ono-jgE"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/z1GZmEfn4_s/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDSBZeNS9TFlHbNJEBtafnHQ5Pb5g",
+        "title" : "OMEGLE VIBES...😍 | B Like Vicky",
+        "name" : "B Like Vicky",
+        "views" : "120K views &bull; 1 month ago",
+        "link" : "https://www.youtube.com/watch?v=z1GZmEfn4_s&t=2s"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/Ojb8OQCBqFs/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC9DrZ5Nl7fxW5rK3a70x4Sf8Ff_w",
+        "title" : "Make Your Desktop Look Clean and Professional in Just 12 Minutes! by Tech Enthusiast 3,236,943 views 8 months ago 12 minutes, 21 seconds",
+        "name" : "Tech Enthusiast",
+        "views" : "3.2M views &bull; 8 months ago",
+        "link" : "https://www.youtube.com/watch?v=Ojb8OQCBqFs"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/tQG6jYy9xto/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBzjlLgx403yIkfVUjEqPubjqjUzw",
+        "title" : "Day in the Life of a Software Engineer - First Person View - Working at the Kahoot Office",
+        "name" : "Marko",
+        "views" : "2.7M views &bull; 1 year ago",
+        "link" : "https://www.youtube.com/watch?v=tQG6jYy9xto&t=511s"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/QCt_wU5j4L8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD6tqHRmUZTclA0gclcs5T5TS1ilA",
+        "title" : "1600HP Lamborghini Essenza SCV12 - Forza Horizon 5 | Thrustmaster TX",
+        "name" : "Xpertgamingtech",
+        "views" : "108K views &bull; 1 month ago",
+        "link" : "https://www.youtube.com/watch?v=QCt_wU5j4L8"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/GAZVvpjxYQY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBfBM33Bjg9Nv9IBLPJavAZnYrHZQ",
+        "title" : "HTML And CSS Tutorial For Beginners | Complete HTML & CSS Course In One Tutorial",
+        "name" : "GreatStack",
+        "views" : "230K views &bull; 3 years ago",
+        "link" : "https://www.youtube.com/watch?v=GAZVvpjxYQY"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/ohnVWZjSejE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgbBfbUHOxYYjy7y_bETrVZG9Dgw",
+        "title" : "Nira Video Song (Extended Version) | Takkar (Tamil) | Siddharth | Karthik G Krish | Nivas K Prasanna",
+        "name" : "Think Music India",
+        "views" : "2.3M views &bull; 2 weeks ago",
+        "link" : "https://www.youtube.com/watch?v=ohnVWZjSejE"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/hraAvEwZ3o4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCo0zqi7VLvFu1hg6Hua0tVRx9Log",
+        "title" : "Variables in JavaScript | Ultimate JavaScript Course #2",
+        "name" : "ProgrammingWithHarry",
+        "views" : "7K views &bull; 1 year ago",
+        "link" : "https://www.youtube.com/watch?v=hraAvEwZ3o4"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/bT__5naNFlI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB7ycXXOC9QfDKOxOPHFewqdIKYGA",
+        "title" : "iPhone 15 Pubg Test, Heating and Battery Test | Gaming Beast 💪",
+        "name" : "Harsh Gamer",
+        "views" : "136K views &bull; 1 day ago",
+        "link" : "https://www.youtube.com/watch?v=bT__5naNFlI"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/EFk9SxwNweQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC99Ibxjt7cqrjrXrMuwDFw4xHbSg",
+        "title" : "Rubik's Cubes From 1$ to 100$",
+        "name" : "CUBASTIC",
+        "views" : "16M views &bull; 5 months ago",
+        "link" : "https://www.youtube.com/watch?v=EFk9SxwNweQ"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/iSnP5c997Uk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDeAjkj0bM7kSLmOW0Kpw4OhWVRYw",
+        "title" : "2021 Mercedes-Maybach S580 | POV NIGHT DRIVE",
+        "name" : "EUROCARGUY",
+        "views" : "8.9M views &bull; 1 year ago",
+        "link" : "https://www.youtube.com/watch?v=iSnP5c997Uk"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/qSaD66vxmqw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBb8hNkaAZ65peToLn_woNXgIIIYQ",
+        "title" : "iOS 17 - 25 Settings You NEED to Change Immediately !",
+        "name" : "iReviews",
+        "views" : "563K views &bull; 5 days ago",
+        "link" : "https://www.youtube.com/watch?v=qSaD66vxmqw"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/_pFvX8xk4CU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA891kw1igbqE4ORbY8VDiM-ka9hw",
+        "title" : "Redmagic 8S Pro - உலகிலையே MOST Powerful Phone - Unboxing (Tamil | தமிழ்)",
+        "name" : "C4ETech Tamil",
+        "views" : "129K views &bull; 1 month ago",
+        "link" : "https://www.youtube.com/watch?v=_pFvX8xk4CU&t=11s"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/ZXl0Iug0iKU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCD_EsT4JGnWo1puj8xiQAmTJeJJg",
+        "title" : "First Ninja H2R Top Speed",
+        "name" : "TestimKay",
+        "views" : "970K views &bull; 1 month ago",
+        "link" : "https://www.youtube.com/watch?v=ZXl0Iug0iKU"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/hTYsf6bdSJ8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD41CLozz10gAhveY1iewRWRnWwig",
+        "title" : "Git & Github Explained in Tamil",
+        "name" : "CS in Tamil",
+        "views" : "343K views &bull; 2 years ago",
+        "link" : "https://www.youtube.com/watch?v=hTYsf6bdSJ8"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/ELlEVBh1xk8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDrxQufyi4doI5m-Ylbqu7wj-ulhg",
+        "title" : "'SAMURAI 2' Photo Manipulation Speed Art | Photoshop Tutorial",
+        "name" : "Learning Screen",
+        "views" : "85K views &bull; 4 weeks ago",
+        "link" : "https://www.youtube.com/watch?v=ELlEVBh1xk8"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/dqlO6_5rZSQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA0lckYqJaQsFj2XoeJsnOlXXO3cA",
+        "title" : "I tried 50 Programming Courses. Here are Top 5.",
+        "name" : "Power Couple",
+        "views" : "934K views &bull; 2 months ago",
+        "link" : "https://www.youtube.com/watch?v=dqlO6_5rZSQ"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/CMKZPlW6woE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBnCwhOfpyziyXdFws0BnXPCLi1Mg",
+        "title" : "Hill Climb Racing 1 - FAST CAR in HIGHWAY | Gameplay Walkthrough",
+        "name" : "Vereshchak",
+        "views" : "2.6M views - 1 year ago",
+        "link" : "https://www.youtube.com/watch?v=CMKZPlW6woE"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/RkivRN8NuMY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBYOAxVeUkFMUv7boLBAR4YZhuiPg",
+        "title" : "Samsung Galaxy S23 Ultra Unboxing - ASMR",
+        "name" : "Dr Debox",
+        "views" : "2.8M views &bull; 6 months ago",
+        "link" : "https://www.youtube.com/watch?v=RkivRN8NuMY"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/Qmhv1_Z7MIA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAtvjXVoEgrbggnzJ9UbZmZvgXcsw",
+        "title" : "Best UI UX Design Course | Best UI UX Training | How to Learn UI UX Design | Intellipaat",
+        "name" : "Intellipaat",
+        "views" : "77K views &bull; Streamed 7 months ago",
+        "link" : "https://www.youtube.com/watch?v=Qmhv1_Z7MIA"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/m1i4r8r17-E/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCeqX40hq9lPSOexNrDYYxNlXZULw",
+        "title" : "Why I Quit Pubg 😥",
+        "name" : "Panda",
+        "views" : "804K views &bull; 2 weeks ago",
+        "link" : "https://www.youtube.com/watch?v=m1i4r8r17-E"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/bFvfqUMjvsA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBISux66ixhGQ68A2xwG_9wMXRIsA",
+        "title" : "Responsive Sidebar Menu in HTML CSS & JavaScript | Dark/Light Mode",
+        "name" : "CodingLab",
+        "views" : "795K views &bull; 1 year ago",
+        "link" : "https://www.youtube.com/watch?v=bFvfqUMjvsA&list=PLImJ3umGjxdCjoBGj1eGQwcopR0P0edAK"
+    },
+    {
+        "image" : "https://i.ytimg.com/vi/CPlisjZ8CNw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCpnp5lPCwD3UBVj7713xEZs35YqA",
+        "title" : "iPhone 15 Pro Max vs Samsung S23 Ultra Camera Battle!",
+        "name" : "Mrwhosetheboss",
+        "views" : "3.5M views &bull; 2 days ago",
+        "link" : "https://www.youtube.com/watch?v=CPlisjZ8CNw"
+    },
+
+]
+
+export default content
